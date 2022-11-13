068a65987e9a119c124929767072b5b489c1626f
xSevithx <sevithstarr@gmail.com>
2022-11-12 20:21:50 -0600

    Init
    update

---
+++ Decimals.sol
@@ -0,0 +1,22 @@
+interface IERC20 {
+    function decimals() external view returns (uint8);
+}
+
+contract DecimalsTest {
+    
+    function changeDecimals(address _fromToken, address _desToken, uint _amount) external view returns (uint) {
+        return convertDecimals(_amount, getDecimals(_fromToken), getDecimals(_desToken));
+    }
+
+    function getDecimals(address _token) internal view returns (uint8) {
+        return IERC20(_token).decimals();
+    }
+
+    function convertDecimals(uint _tokenAmount, uint8 _token1decimals, uint8 _token2decimals) internal pure returns (uint) {
+        if (_token1decimals > _token2decimals)
+            return _tokenAmount / (10**(_token1decimals - _token2decimals));
+        else
+            return _tokenAmount * (10**(_token2decimals - _token1decimals));
+    }
+
+}
